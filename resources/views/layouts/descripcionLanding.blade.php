4f73cbadfa6f17fab13d7daf5ae195810923dfdb
Juangui <jgmejiaco@gmail.com>
2026-07-15 12:41:02 -0500

    descripción storedimo Landing

--- resources/views/layouts/descripcionLanding.blade.php
+++ resources/views/layouts/descripcionLanding.blade.php
@@ -1,7 +1,6 @@
 <div class="">
-    <p class="" style="text-align: justify;">
-        Es un sistema de inventario en la nube que le permite llevar el control de su negocio de forma fácil, organizada y disponible.
-        Cada empresa o persona administra de manera independiente y en un solo lugar, sus clientes, proveedores, categorías, productos, compras, ventas y existencias.
-        Además, puede registrar las bajas de productos por daño, hurto o vencimiento, y gestionar su suscripción según el plan que mejor se adapte a sus necesidades.
-    </p>
+    <p style="text-align: justify;">Es un sistema de inventario en la nube que le permite llevar el control de su negocio de forma fácil, organizada y disponible.</p>
+    <p style="text-align: justify;">Cada empresa o persona administra de manera independiente y en un solo lugar, sus clientes, proveedores, categorías, productos, compras, ventas y existencias.</p>
+    <p style="text-align: justify;">Además, puede registrar las bajas de productos por daño, hurto o vencimiento.</p>
+    <p style="text-align: justify;">Puede también gestionar su suscripción según el plan que mejor se adapte a sus necesidades.</p>
 </div>
